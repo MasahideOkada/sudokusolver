861bbd2eee0c5f06e249a97f5ffdb48debbccc9f
MasahideOkada <masahideokada1121@gmail.com>
2022-07-03 07:59:27 +0900

    add name.hpp

---
+++ include/names.hpp
@@ -0,0 +1,25 @@
+#ifndef NAMES_HPP
+#define NAMES_HPP
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> region_names = {
+    "row", 
+    "column", 
+    "box"
+};
+
+std::vector<std::string> tuple_names = {
+    "single",
+    "pair",
+    "triple",
+    "quad",
+    "quint",
+    "hexad",
+    "heptad",
+    "octad",
+    "nonad"
+}
+
+#endif
